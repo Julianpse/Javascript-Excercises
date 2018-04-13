101d633fe764eebd98d027d6dd3cd86b10daa9b5
Julianpse <julianpse@gmail.com>
2018-04-12 23:22:15 -0500

    finished next exercise

---
+++ js-exercises-1/printABox.js
@@ -0,0 +1,3 @@
+function printBox(width, height){
+
+}
---
+++ js-exercises-1/printSquare.js
@@ -0,0 +1,10 @@
+function printSquare(size){
+  star = "*".repeat(size);
+  for(i = 0; i < size; i++){
+      console.log(star);
+  }
+}
+
+printSquare(5);
+
+// Not finished
