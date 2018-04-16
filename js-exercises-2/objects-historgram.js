4c729edad606dfc1e67adceba63249a741c98d52
Julianpse <julianpse@gmail.com>
2018-04-15 19:39:02 -0500

    finished exercise 1

---
+++ js-exercises-2/objects-historgram.js
@@ -0,0 +1,8 @@
+//Letter Histogram
+//Write a function letterHistogram which takes a string as argument.
+//It will tally(histogram) the number of times each character appears in the string,
+//and return the tally as an object.
+function letterHistogram (string) {
+  string.split('')
+
+}
